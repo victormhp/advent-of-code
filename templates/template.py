0d0955b59d3ef3d01fe54464d0164cfd590ccce1
Victor Alberto <victoralbertomhp@gmail.com>
2022-12-09 11:58:46 -0600

    Add templates

---
+++ templates/template.py
@@ -0,0 +1,18 @@
+def get_input():
+    with open("./input.txt") as f:
+        input = f.read().splitlines()
+    return input
+
+    
+def part1(input):
+    pass
+
+
+
+def part2(input):
+    pass
+
+
+input = get_input()
+print(f"Part 1: {part1(input)}")
+print(f"Part 2: {part2(input)}")
